016e455f5a87734f7034da2b6df6d927666ed845
Greg Miller <w4ik@arrl.net>
2011-03-27 16:03:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plasticmodeldb::Application.config.secret_token = '6f3a81c9ce43a275f6fe76afb542e0204bb7162be86a4cda3ed84b22fe25c1c0f636f4bf83272976067f14b713170206b13f3eaf0a4715d0621b5a9474faa66d'
